8c581cbb220bb92e80dbbe27ca0942878962edde
deepaksoni47 <deepaksoni23022004@gmail.com>
2025-12-17 14:06:35 +0530

    landing page

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f9e483bf5ba44e93"; }
+/******/ 		__webpack_require__.h = function() { return "01d3d6d9c47c3474"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
